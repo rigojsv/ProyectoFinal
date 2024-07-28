a03810eeb89353fb972f36f084689bdbee76b66d
SrGomita <150401993+SrGomita@users.noreply.github.com>
2024-07-27 22:24:32 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -25,6 +25,24 @@ const handlerBarsContext = {
             "image": "resources/events/img5.jpg",
             "alt": "Event img 5"
         }
+    ],
+    "faqs": [
+        {
+            "question": "¿Cuál es la política de devoluciones?",
+            "answer": "Nuestra política de devoluciones permite que los clientes devuelvan los productos dentro de los 30 días siguientes a la compra, siempre que los artículos estén en su embalaje original y en condiciones de reventa."
+        },
+        {
+            "question": "¿Cuáles son los métodos de pago aceptados?",
+            "answer": "Aceptamos efectivo, tarjetas de crédito, débito y transferencias bancarias."
+        },
+        {
+            "question": "¿Cuentan con envío a domicilio?",
+            "answer": "Si, tenemos envíos gratis a su domicilio en la ciudad de Juticalpa, y hacemos envíos a todo el país con la agencia de logística C807Xpress."
+        },
+        {
+            "question": "¿Cuánto tarda y cuál es el costo de envío?",
+            "answer": "El costo de envío es de Lps.90 y tarda de 1-3 días en llegar a su domicilio."
+        }
     ]
 };
 
@@ -33,3 +51,4 @@ const pageContext = () => {
 };
 
 export default pageContext;
+
