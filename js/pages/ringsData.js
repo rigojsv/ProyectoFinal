0e93af873d07c6ace983b7db2d432b5225526988
LuisMz04 <157434146+LuisMz04@users.noreply.github.com>
2024-07-28 14:53:58 -0600

    Add files via upload

---
+++ js/pages/ringsData.js
@@ -0,0 +1,73 @@
+const ringsData = {
+    title: 'Anillos',
+    products: [
+        {
+            img: 'resources/rings/img1.jpeg',
+            alt: 'Anillos varios',
+            title: 'Anillos',
+            price: 'L.180 c/u'
+        },
+        {
+            img: 'resources/rings/img2.jpg',
+            alt: 'Anillos de gatitos',
+            title: 'Anillos de gatitos',
+            price: 'L.150 c/u'
+        },
+        {
+            img: 'resources/rings/img3.jpg',
+            alt: 'Anillos de rayo',
+            title: 'Anillos de rayo',
+            price: 'L.160 c/u'
+        },
+        {
+            img: 'resources/rings/img4.jpg',
+            alt: 'Anillos con diseño de araña',
+            title: 'Anillos con diseño de araña',
+            price: 'L.150 c/u'
+        },
+        {
+            img: 'resources/rings/img5.jpg',
+            alt: 'Anillo varios',
+            title: 'Anillo',
+            price: 'L.150 c/u'
+        },
+        {
+            img: 'resources/rings/img6.jpg',
+            alt: 'Anillo con huella',
+            title: 'Anillo con huella',
+            price: 'L.170 c/u'
+        },
+        {
+            img: 'resources/rings/img7.jpg',
+            alt: 'Anillo sol y luna',
+            title: 'Anillo sol y luna',
+            price: 'L.160 c/u'
+        },
+        {
+            img: 'resources/rings/img8.jpg',
+            alt: 'Anillos de araña',
+            title: 'Anillos de araña',
+            price: 'L.160 c/u'
+        },
+        {
+            img: 'resources/rings/img9.jpg',
+            alt: 'Anillos dinosaurio',
+            title: 'Anillos dinosaurio',
+            price: 'L.150 c/u'
+        },
+        {
+            img: 'resources/rings/img10.jpg',
+            alt: 'Anillo corazón',
+            title: 'Anillo corazón',
+            price: 'L.150 c/u'
+        },
+        {
+            img: 'resources/rings/img11.jpg',
+            alt: 'Anillo con bolita azul',
+            title: 'Anillo con bolita azul',
+            price: 'L.160 c/u'
+        }
+    ]
+};
+
+export default ringsData;
